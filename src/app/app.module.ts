f0125271dbbe258ea7a2c9a08ab79429e88ba4fd
jwendell-at-036205687955 <IPrNT7YGrqDLDScijz6QpVL+pG2x+H+iENGTBVTpfcA=>
2021-11-04 10:29:35 +0000

    start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { MediaLibraryComponent } from './views/media-library/media-library.component';
+import {MediaLibraryComponent } from './views/media-library/media-library.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatListModule} from '@angular/material/list';
 import * as echarts from 'echarts';
-import { NgxEchartsModule } from 'ngx-echarts';
+import {NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
