fa8da4c01ad8d7dc64919ecd61ac9431b7898dce
jwendell-at-036205687955 <IPrNT7YGrqDLDScijz6QpVL+pG2x+H+iENGTBVTpfcA=>
2021-11-11 00:57:19 +0000

    some updates

--- src/app/start-menu/start-menu.component.ts
+++ src/app/start-menu/start-menu.component.ts
@@ -1,18 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+
+
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Country Profile',
+    children: [{name: 'Profile 1'}, {name: 'Profile 2'}, {name: 'Profile 3'}],
+  },
+  {
+    name: 'Industry Profile',
+    children: [{name: 'Finance'}, {name: 'Retail'}, {name: 'Space'}],
+  },
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
 
 @Component({
   selector: 'app-start-menu',
   templateUrl: './start-menu.component.html',
   styleUrls: ['./start-menu.component.scss']
 })
+
+
 export class StartMenuComponent implements OnInit {
   
   typesOfMarkets: string[] = ['Argentina', 'Australia', 'Austria', 'Bahamas', 'Bahrain', 'Bangladesh ', 'Barbados', 'Belgium ', 'Belize', 'Benin', 'Bermuda', 'Bolivia' ];
 
-  constructor() { }
+
 
   ngOnInit(): void {
   }
 
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
 
