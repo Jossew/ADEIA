ec53ff7e41fcd572ea6c85cea8dea64bbe8e8185
jwendell-at-036205687955 <IPrNT7YGrqDLDScijz6QpVL+pG2x+H+iENGTBVTpfcA=>
2021-11-15 21:46:22 +0000

    score

--- src/app/views/summary/summary.component.ts
+++ src/app/views/summary/summary.component.ts
@@ -21,6 +21,8 @@ export class SummaryComponent implements OnInit {
  private genderPie: any = null;
  private ethnicityPie: any = null;
 
+ private diversityScore: any = null;
+
 
   ngOnInit() {
    this.InitPipe();
@@ -33,12 +35,13 @@ export class SummaryComponent implements OnInit {
     this.agePie = echarts.init((document.getElementById('piechartage')) as any);
     this.genderPie = echarts.init((document.getElementById('piechartgender')) as any);
     this.ethnicityPie = echarts.init((document.getElementById('piechartetnicity')) as any);
+    this.diversityScore = echarts.init((document.getElementById('scorechart')) as any);
 
 
 const fontWeight = {
   fontWeight: 'bold',
 };
-const color = {color:'#5B34D5'};
+const color = {};
 const piecharts = {
   tooltip: {
     trigger: 'item'
@@ -49,20 +52,19 @@ const piecharts = {
     title: [
     {
       left: 'center',
-      text: 'Gradient along the y axis'
-    },
-    {
-      top: '55%',
-      left: 'center',
-      text: 'Gradient along the x axis'
+      text: 'Chart Title',
+      textStyle: {
+        fontSize: 14,
+        color: '#fff'
     }
+    },
   ],
-  color:[ '#5B34D5','#fff','#FF9800','#81C784','#c4ccd3','#c23531','#2f4554','#61a0a8','#d48265','#91c7ae',  '#A5224A',
-
-  '#FFF18C'],
+  color:[ 
+    '#5B34D5','#fff','#FF9800','#81C784','#c4ccd3','#c23531','#2f4554','#61a0a8','#d48265','#91c7ae',  '#A5224A',
+  ],
   series: [
     {
-      name: 'Access From',
+      name: 'Diversity',
       type: 'pie',
       radius: ['40%', '70%'],
       avoidLabelOverlap: false,
@@ -89,9 +91,107 @@ const piecharts = {
     }
   ]
 };
+
+
+
+
+const gaugeData = [
+  {
+    value: 20,
+    name: 'Perfect',
+    title: {
+      offsetCenter: ['0%', '-30%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '-20%']
+    }
+  },
+  {
+    value: 40,
+    name: 'Good',
+    title: {
+      offsetCenter: ['0%', '0%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '10%']
+    }
+  },
+  {
+    value: 60,
+    name: 'Commonly',
+    title: {
+      offsetCenter: ['0%', '30%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '40%']
+    }
+  }
+];
+
+const scorechart = {
+  series: [
+    {
+      type: 'gauge',
+      startAngle: 90,
+      endAngle: -270,
+      pointer: {
+        show: false
+      },
+      progress: {
+        show: true,
+        overlap: false,
+        roundCap: true,
+        clip: false,
+        itemStyle: {
+          borderWidth: 1,
+          borderColor: '#464646'
+        }
+      },
+      axisLine: {
+        lineStyle: {
+          width: 40
+        }
+      },
+      splitLine: {
+        show: false,
+        distance: 0,
+        length: 10
+      },
+      axisTick: {
+        show: false
+      },
+      axisLabel: {
+        show: false,
+        distance: 50
+      },
+      data: gaugeData,
+      title: {
+        fontSize: 14
+      },
+      detail: {
+        width: 50,
+        height: 14,
+        fontSize: 14,
+        color: 'auto',
+        borderColor: 'auto',
+        borderRadius: 20,
+        borderWidth: 1,
+        formatter: '{value}%'
+      }
+    }
+  ]
+};
+
+
+
+
     this.agePie.setOption(piecharts);
     this.genderPie.setOption(piecharts);
     this.ethnicityPie.setOption(piecharts);
+    this.diversityScore.setOption(scorechart);
  }
   
   
